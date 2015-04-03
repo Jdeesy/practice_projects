50057cdfdeda7a7e6336d5f6037fb6bb81667618
jdeesy <jdees08@gmail.com>
2015-04-03 10:24:35 -0500

    adding rough draft of finished solution

--- tic-tac-toe/tic_tac_toe.rb
+++ tic-tac-toe/tic_tac_toe.rb
@@ -17,7 +17,7 @@
 
 class TicTacToe
   attr_accessor :game_board
-  WINNING_COMBOS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,9]]
+  WINNING_COMBOS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 
   def initialize
     @game_board = {1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 => " ", 8 => " ", 9 => " "}
@@ -43,28 +43,35 @@ class TicTacToe
   end
 
   def game_controller
-    until winner?
+    display_board
+    @winner = false
+    until @winner == true
       @players.each do |player|
+        winner?
         empty_cell = false
-        display_board
-        display_current_board
+        break if @winner == true
+
 
         while empty_cell == false
           puts "#{player}'s please select a cell"
-          cell = gets.chomp.downcase
+          player_input = gets.chomp.downcase
 
-          if cell == "exit" || cell == "q" || cell == "quit"
-            abort
-          elsif cell.to_i != 0
-            if @game_board[cell.to_i] == " "
-              game_board[cell.to_i] = "#{player}"
+          if player_input == "exit" || player_input == "q" || player_input == "quit"
+            puts "Are you sure you want to quit? (y/n)"
+            answer = gets.chomp.downcase
+            abort if answer == "y" || answer == "yes"
+          elsif player_input.to_i != 0
+            if @game_board[player_input.to_i] == " "
+              game_board[player_input.to_i] = "#{player}"
               empty_cell = true
+              display_current_board
             else
               puts "I am sorry but that cell already has an X or O. Please select again."
               display_current_board
             end
           else
-            puts "Sorry I don't reconize that command. Please try again."
+            puts "Sorry I don't recognize that command. Please try again."
+            display_board
             display_current_board
           end
         end
@@ -73,12 +80,22 @@ class TicTacToe
   end
 
   def winner?
-    false
+    WINNING_COMBOS.each do |cells|
+      winning_array = []
+      cells.each { |cell| winning_array << game_board[cell] }
+      if winning_array.all? { |x| x.upcase == "X" }
+        puts "The winner is X!"
+        @winner = true
+      elsif winning_array.all? { |o| o.upcase == "O"}
+        puts "The winner is O!"
+        @winner = true
+      end
+    end
   end
 
-  def to_s
-    "#{game_board[1]}|#{game_board[2]}|#{game_board[3]}\n-----\n#{game_board[4]}|#{game_board[5]}|#{game_board[6]}\n-----\n#{game_board[7]}|#{game_board[8]}|#{game_board[9]}"
-  end
+  # def to_s
+  #   "#{game_board[1]}|#{game_board[2]}|#{game_board[3]}\n-----\n#{game_board[4]}|#{game_board[5]}|#{game_board[6]}\n-----\n#{game_board[7]}|#{game_board[8]}|#{game_board[9]}"
+  # end
 end
 
 game = TicTacToe.new
